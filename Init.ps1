bbbc5094add5b11a94bd75952dbb0fe5e6657bf3
Jeferson Barrero <jebarrero14@gmail.com>
2025-02-03 21:34:20 -0500

    Enhance Init.ps1 script with improved output messages and directory validation

--- Init.ps1
+++ Init.ps1
@@ -1,31 +1,57 @@
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $venvPath = Join-Path $scriptDir "venv"
 $scriptPath = Join-Path $scriptDir "main.py"
+$requirementsPath = Join-Path $scriptDir "requirements.txt"
+$allInstalled = $true
 
 if (-not (Test-Path $venvPath)) {
-    Write-Output "Creating virtual environment..."
+    Write-Host "Creating virtual environment..." -ForegroundColor Yellow
     python -m venv $venvPath
 }
 
-Write-Output "Create directory for saving files renamed..."
-mkdir $scriptDir\renombrados -ErrorAction SilentlyContinue
+Write-Host "Environment created successfully" -ForegroundColor Green
 
-Write-Output "validating if the excel file exists..."
+if (-not(Test-Path $scriptDir\renombrados)) {
+    Write-Host "Create directory for saving files renamed..." -ForegroundColor Yellow
+    mkdir $scriptDir\renombrados 
+}
+
+Write-Host "Directory renombrados created successfully" -ForegroundColor Green
+
+Write-Host "validating if the excel file exists..." -ForegroundColor Yellow
 
 if (-not (Test-Path $scriptDir\soportes.xlsx)) {
-    Write-Output "The file soportes.xlsx does not exist, please add it to the root of the project"
+    Write-Host "The file soportes.xlsx does not exist, please add it to the root of the project" -ForegroundColor Red
     exit
 }
 
-Write-Output "Activating virtual environment..."
-Write-Output "venvPath: $venvPath"
+Write-Host "Activating virtual environment..." -ForegroundColor Yellow
+Write-Host "venvPath: $venvPath"
 . $venvPath\Scripts\Activate.ps1
+Write-Host "Virtual environment activated" -ForegroundColor Green
 
-Write-Output "Installing dependencies..."
-pip install -r $scriptDir\requirements.txt
+foreach ($line in Get-Content $requirementsPath) {
+    $package = $line.Split('==')[0]
+    $installed = pip show $package
 
-Write-Output "Running script..."
+    if (-not $installed) {
+        Write-Host "Installing $package..." -ForegroundColor Yellow
+        pip install $line
+        $allInstalled = $false
+    }
+
+}
+
+if ($allInstalled) {
+    Write-Host "All packages are already installed" -ForegroundColor Green
+}
+else {
+    Write-Host "All packages installed successfully" -ForegroundColor Green
+    pip install -r $requirementsPath
+}
+
+Write-Host "Running script..." -ForegroundColor Yellow
 python $scriptPath
 
-Write-Output "Deactivating virtual environment..."
+Write-Host "Deactivating virtual environment..."
 deactivate
